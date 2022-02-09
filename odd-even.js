50241a49cdd91f9a5642b6c3903d927bf970d8e6
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-02-09 17:49:19 +0600

    Added two file

---
+++ odd-even.js
@@ -0,0 +1,25 @@
+const number = 11;
+function isEven(number)
+{
+    if (number % 2 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+// console.log(isEven(number))
+
+function isOdd(number)
+{
+    if (number % 2 != 0)
+    {
+        return true;
+    }
+
+    return true;
+};
+
+console.log(isOdd(number));
+
+
